6d2638e1b4d6a2a41ffa7ce741be608c66913a00
vicmafe <victormateuslf@gmail.com>
2021-05-09 21:56:16 -0300

    input in navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -8,39 +8,56 @@ import Button from '../Button'
 const Navbar = () => {
   const [searchInput, setSearchInput] = useState(false);
   const [color, setColor] = useState();
+  const colorIconCliqued = "invert(37%) sepia(97%) saturate(2412%) hue-rotate(312deg) brightness(99%) contrast(105%)";
+  const searchMovies = () => {
+    return console.log('digitando...')
+  }
+  const updateInput = () => {
+    if (searchInput) {
+      setColor("invert(100%)");
+      return setSearchInput(false);
+    };  
+    setColor(colorIconCliqued);
+    return setSearchInput(true);
+  }
   useEffect(()=> {
-    setColor(
-      "invert(37%) sepia(97%) saturate(2412%) hue-rotate(312deg) brightness(99%) contrast(105%)"
-    );
-  }, [searchInput])
+    
+  }, [searchInput]);
   return (
-    <S.Container>
-      <S.Image
-        src={ Logo }
-        alt="logo suno movies"
-      />
-      <S.ContainerButtons>
-        <S.Button>
-          <Button
-            onClick={ () => <Redirect to="/" />}
-          >
-            INÍCIO
-          </Button>
-        </S.Button>
-        <S.Button>
-          <Button>
-            CATÁLOGO
-          </Button>
-        </S.Button>
-        <S.Icon
-          invertColor={color}
-          onClick={ () => setSearchInput(true)}
-          src={ IconSearch }
-          alt=" lupa icone"
+    <S.ContainerNav>
+      <S.ContainerMenu>
+        <S.Image
+          src={ Logo }
+          alt="logo suno movies"
         />
-      </S.ContainerButtons>
-      {!searchInput ? <></> : <h1>Ok</h1> }
-    </S.Container>
+        <S.ContainerButtons>
+          <S.Button>
+            <Button
+              onClick={ () => <Redirect to="/" />}
+            >
+              INÍCIO
+            </Button>
+          </S.Button>
+          <S.Button>
+            <Button>
+              CATÁLOGO
+            </Button>
+          </S.Button>
+          <S.Icon
+            invertColor={ color }
+            onClick={ () => updateInput()}
+            src={ IconSearch }
+            alt=" lupa icone"
+          />
+        </S.ContainerButtons>
+      </S.ContainerMenu>
+        {!searchInput ? <></> : 
+          <S.InputSearch
+            type="text"
+            onchange={({target}) => searchMovies(target.value)}
+          />
+        }
+    </S.ContainerNav>
   )
 };
 
--- src/Components/Navbar/style.js
+++ src/Components/Navbar/style.js
@@ -1,25 +1,36 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const ContainerNav = styled.div`
+  width: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #212125;
+`;
+export const ContainerMenu = styled.div`
   display: flex;
   justify-content: space-around;
   background-color: #212125;
   align-items: center;
-  border: 1px solid black;
   width: 100%;
   min-height: 10vh;
+  border-bottom: 1px solid #151518;
 `;
 export const Image = styled.img`
-  margin: 5px;
+  margin-left: 0px;
   display: flex;
   width: 20%;
 `;
 export const Icon = styled.img`
   display: flex;
-  margin: 20px;
-  filter: ${props => props.invertColor};
+  margin-top: 12px;
+  margin-bottom: 12px;
+  margin-left: 15px;
+  filter: ${props => props.invertColor || "invert(100%)"};
   cursor: pointer;
-  width: 7%;
+  width: 20px;
 `;
 export const ContainerButtons = styled.div`
   display: flex;
@@ -33,4 +44,22 @@ export const Button = styled.div`
   &:hover {
     border-bottom: 2px solid #ff388c;
   }
+`;
+export const InputSearch = styled.input`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 10px;
+  width: 70%;
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  border-bottom: 2px solid #ff388c;
+  min-height: 5vh;
+  background-color: #212125;
+  outline: 0;
+  caret-color: #ff388c;
+  color: #fff;
+  font-size: 30px;
 `;

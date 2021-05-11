8c213ec3f91fbe9c94d0468ee876d3766f874096
vicmafe <victormateuslf@gmail.com>
2021-05-11 08:57:51 -0300

    modified url root

--- src/Service/index.js
+++ src/Service/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const apiKey = '7090ec6c8631c6473ca8d07d9869fe92';
-const url = 'https://api.themoviedb.org/3';
-const releasesMoviesURL = `${url}/movie/upcoming`;
-const moviesUrl = `${url}/discover/movie`;
-const popularMoviesUrl = `${url}/movie/popular`;
+const urlRoot = 'https://api.themoviedb.org/3';
+const releasesMoviesURL = `${urlRoot}/movie/upcoming`;
+const moviesUrl = `${urlRoot}/discover/movie`;
+const popularMoviesUrl = `${urlRoot}/movie/popular`;
 
-const params= {
+const params = {
   api_key: apiKey,
   language: 'en-US',
   page: 1
@@ -14,16 +14,16 @@ const params= {
 
 export const GetReleasesMovies = async () => {
   const { data } = await axios.get(releasesMoviesURL, { params });
-  return data.results
+  return data.results;
 };
 
 export const GetAllMovies = async () => {
   const { data } = await axios.get(moviesUrl, { params });
-  return data.results
+  return data.results;
 };
 
 export const GetPopularMovies = async () => {
   const { data } = await axios.get(popularMoviesUrl, { params });
-  return data.results
+  return data.results;
 };
 

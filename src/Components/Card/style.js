1ee321dbd9ccfc5e77ef01012836e08b59e3b103
vicmafe <victormateuslf@gmail.com>
2021-05-11 10:52:00 -0300

    create card component

---
+++ src/Components/Card/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+  margin: 3px;
+  border-radius: 3px;
+  display: flex;
+  width: 20%;
+`;
+export const Title = styled.h5`
+  display: flex;
+  margin: 1px;
+  color: #fff;
+`;
+export const Genre = styled.p`
+  display: flex;  
+  font-size: 8px;
+  margin: 1px;
+  color: #ff388c;
+`;
+export const Rate = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+export const Overview = styled.p`
+  display: flex;
+  font-size: 8px;
+  margin: 1px;
+  color: #ffff;
+`;
+export const Star = styled.img`
+  margin: 1px;
+  display: flex;
+  width: 5px;
+  filter: "invert(37%) sepia(97%) saturate(2412%) hue-rotate(312deg) brightness(99%) contrast(105%)";
+`;

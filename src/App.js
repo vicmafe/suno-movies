338d355441d21385e9eade9ba379cd52708624ef
vicmafe <victormateuslf@gmail.com>
2021-05-13 15:29:09 -0300

    add extension globalstyle

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Provider from './Context/Provider'
 import Home from './Pages/Home';
-import GlobalStyle from './Style/GlobalStyle'
+import GlobalStyle from './Style/GlobalStyle.js'
 
 function App() {
   return (
@@ -16,5 +16,4 @@ function App() {
     </Provider>
   );
 }
-
 export default App;
